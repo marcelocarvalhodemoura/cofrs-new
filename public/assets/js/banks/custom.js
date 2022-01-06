df9ce1cbf95a5462d4809218b041266b1c25b9f0
John <jonatascrizel@gmail.com>
2022-01-06 17:48:37 -0300

    Menu contabilidade > bancos

---
+++ public/assets/js/banks/custom.js
@@ -0,0 +1,176 @@
+$(document).ready(function () {
+
+  var table = $('#table').DataTable({
+    dom: "<'dt--top-section'<'row'<'col-12 col-sm-6 d-flex justify-content-sm-start justify-content-center'l>" +
+      "<'col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center mt-sm-0 mt-3'f>>>" +
+      "<'table-responsive'tr>" +
+      "<'dt--bottom-section d-sm-flex justify-content-sm-between text-center'<'dt--pages-count  mb-sm-0 mb-3'i><'dt--pagination'p>>",
+    processing: true,
+    serverSide: true,
+    ajax: "/banks",
+    columns: [
+      { data: 'name_bank', name: 'name_bank' },
+      { data: 'bank_agency', name: 'bank_agency' },
+      { data: 'bank_account', name: 'bank_account' },
+      { data: 'action', name: 'action', orderable: false, searchable: false },
+    ],
+    "oLanguage": {
+      "sProcessing": "Processando...",
+      "sLengthMenu": "Mostrar _MENU_ registros",
+      "sZeroRecords": "Não foram encontrados resultados",
+      "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+      "sInfoEmpty": "Mostrando de 0 até 0 de 0 registros",
+      "sInfoFiltered": "",
+      "sInfoPostFix": "",
+      "pagingType": "full_numbers",
+      "sSearch": "Buscar:",
+      "sUrl": "",
+      "oPaginate": {
+        "sPrevious": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-arrow-left\"><line x1=\"19\" y1=\"12\" x2=\"5\" y2=\"12\"></line><polyline points=\"12 19 5 12 12 5\"></polyline></svg>",
+        "sNext": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-arrow-right\"><line x1=\"5\" y1=\"12\" x2=\"19\" y2=\"12\"></line><polyline points=\"12 5 19 12 12 19\"></polyline></svg>"
+      }
+    },
+  });
+
+  /**
+   * Form add 
+   */
+  $("#formItem").validate({
+    rules: {
+      name_bank: "required",
+      bank_agency: "required",
+      bank_account: "required",
+    },
+    messages: {
+      name_bank: "Banco é um campo obrigatório",
+      bank_agency: "Agência é um campo obrigatório",
+      bank_account: "Conta é um campo obrigatório",
+    },
+    errorElement: "span",
+    highlight: function () {
+      $("#formItem").addClass("was-validated");
+    },
+    unhighlight: function () {
+      $("#formItem").addClass("was-validated");
+    },
+    submitHandler: function () {
+      $.ajax({
+        url: '/banks/store',
+        method: 'POST',
+        data: $('#formItem').serialize(),
+        success: function (data) {
+
+          if (data.status === 'success') {
+
+            table.ajax.reload();
+
+            $('#userFormModal').modal('hide');
+
+            swal({
+              title: 'Bom trabalho!',
+              text: "Formulário salvo com sucesso",
+              type: 'success',
+            });
+
+            $('#formItem')[0].reset();
+          }
+        }
+
+      });
+    }
+  });
+
+  /**
+   * Form edit 
+   */
+  $("#formItemEdit").validate({
+    rules: {
+      name_bank: "required",
+      bank_agency: "required",
+      bank_account: "required",
+    },
+    messages: {
+      name_bank: "Banco é um campo obrigatório",
+      bank_agency: "Agência é um campo obrigatório",
+      bank_account: "Conta é um campo obrigatório",
+    },
+    errorElement: "span",
+    highlight: function () {
+      $("#formItemEdit").addClass("was-validated");
+    },
+    unhighlight: function () {
+      $("#formItemEdit").addClass("was-validated");
+    },
+    submitHandler: function () {
+      $.ajax({
+        url: '/banks/store',
+        method: 'POST',
+        data: $('#formItemEdit').serialize(),
+        success: function (data) {
+
+          if (data.status === 'success') {
+
+            table.ajax.reload();
+
+            $('#editModal').modal('hide');
+
+            swal({
+              title: 'Bom trabalho!',
+              text: "Formulário salvo com sucesso",
+              type: 'success',
+            });
+
+            $('#formItemEdit')[0].reset();
+          }
+        }
+
+      });
+    }
+  });
+
+  /**
+   * Validate checkbox in the datatables
+   *
+   * @type {any[]}
+   */
+  $('#btnEdit').on('click', function (e) {
+
+    var id = new Array();
+
+    $("input[type=checkbox][name=\'actionCheck[]\']:checked").each(function () {
+      //get value in the input
+      id.push($(this).val());
+      //alert(usuarioMarcados);
+    });
+
+    //validate if exist value
+    if (id > 0) {
+      $.ajax({
+        url: '/banks/load/' + id,
+        method: 'GET',
+        success: function (response) {
+
+          $('input#name_bank.form-control').val(response[0].name_bank);
+          $('input#bank_agency.form-control').val(response[0].bank_agency);
+          $('input#bank_account.form-control').val(response[0].bank_account);
+
+          $('#formItemEdit').append('<input type="hidden" id="id" name="id" value="' + response[0].id + '"/>');
+        }
+      });
+      $('#editModal').modal()
+    } else {
+      swal({
+        title: "Atenção !",
+        text: "Selecione apenas 1 registro por vez",
+        type: "info",
+        confirmButtonClass: 'btn btn-primary',
+      });
+    }
+
+  });
+
+
+
+
+
+});
---
+++ resources/views/banks/modal/create.blade.php
@@ -0,0 +1,27 @@
+<button type="button" class="btn btn-primary bs-popover rounded mb-2 mr-2" data-toggle="modal" data-target="#userFormModal" data-placement="top" data-container="body" data-trigger="hover" data-content="Cadastrar">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+    </svg>
+</button>
+<!-- Modal -->
+<div class="modal fade bd-example-modal-xl" id="userFormModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Formulário Cadastramento de Bancos</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+                        <line x1="18" y1="6" x2="6" y2="18"></line>
+                        <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                </button>
+            </div>
+            <div class="modal-body">
+                @include('banks.form.form')
+
+            </div>
+
+        </div>
+    </div>
+</div>
