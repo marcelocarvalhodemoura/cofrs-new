6b3a2915635621344e835449c26051682ffcc829
John <jonatascrizel@gmail.com>
2022-01-26 15:06:10 -0300

    Gráficos do dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+use App\Models\Cashflow;
+use App\Models\Associate;
+use DateTime;
 
 class DashboardController extends Controller
 {
@@ -13,6 +17,54 @@ class DashboardController extends Controller
             return redirect()->route('login');
         }
 
+        $vigencia = date('m/Y');
+
+        $resumo_operacoes = Cashflow::select('credito', DB::raw("SUM(valor) as valor"))
+        ->where(DB::raw("YEAR(data_vencimento)"), '=', date('Y'))
+        ->where(DB::raw("MONTH(data_vencimento)"), '=', date('m'))
+        ->groupBy('credito')
+        ->get();
+
+        $op_cresc = Cashflow::select('credito', 
+            DB::raw("SUM(valor) as valor"),
+            DB::raw("YEAR(data_vencimento) as ano"),
+            DB::raw("MONTH(data_vencimento) as mes"))
+        ->where(DB::raw("YEAR(data_vencimento)"), '<=', date('Y'))
+        ->where(DB::raw("MONTH(data_vencimento)"), '<=', date('m'))
+        ->groupBy('credito')
+        ->groupBy(DB::raw("YEAR(data_vencimento)"))
+        ->groupBy(DB::raw("MONTH(data_vencimento)"))
+        ->orderbyDesc('data_vencimento')
+        ->limit(12)
+        ->get();
+
+        $crescimento_operacoes = [];
+        //seto um ano antes para garantir que o gráfico terá os 11 meses mesmo que não exista movimentação
+        $dt = new DateTime('first day of -11 months');
+        for ($i = 0; $i < 12; $i++) {
+            $m = $dt->format('m');
+            // preciso fazer isso porque o método retorna uma string
+            settype($m,'integer');
+
+            $crescimento_operacoes[0][$m.'/'.$dt->format('Y')] = 0;
+            $crescimento_operacoes[1][$m.'/'.$dt->format('Y')] = 0;
+            $dt->modify('+1 month');
+        }
+        //preencho o array com os valores que tiver
+        foreach($op_cresc as $item){
+            $crescimento_operacoes[$item->credito][$item->mes.'/'.$item->ano] = $item->valor;
+        }
+        //dd($crescimento_operacoes);
+
+        $associados = Associate::select(DB::raw("COUNT(associado.id) AS quantidade"),DB::raw("COUNT(lancamento.id) AS conveniados"))
+        ->leftJoin('lancamento','assoc_codigoid','=','associado.id')
+        ->where('assoc_ativosn','=',1)
+        ->first();
+
+        $ass_total = $associados->quantidade;
+        $ass_nconveniados = $associados->quantidade - $associados->conveniados;
+        $ass_conveniados = $associados->conveniados;
+
         $data = [
             'category_name' => 'dashboard',
             'page_name' => 'analytics',
@@ -20,6 +72,12 @@ class DashboardController extends Controller
             'offsetTop' => '',
             'scrollspy_offset' => '',
             'alt_menu' => 0,
+            'vigencia' => $vigencia,
+            'resumo_operacoes' => $resumo_operacoes,
+            'ass_total' => $ass_total,
+            'ass_nconveniados' => $ass_nconveniados,
+            'ass_conveniados' => $ass_conveniados,
+            'crescimento_operacoes' => $crescimento_operacoes,
         ];
 
         return view('dashboard')->with($data);
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,28 +9,23 @@
             <div class="col-xl-5 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                 <div class="widget widget-one">
                     <div class="widget-heading">
-                        <h6 class="">Statistics2</h6>
+                        <h6 class="">Resumo das operações em {{$vigencia}}</h6>
                     </div>
                     <div class="w-chart">
+                    @foreach ($resumo_operacoes as $item)
                         <div class="w-chart-section">
-                            <div class="w-detail">
-                                <p class="w-title">Total Visits</p>
-                                <p class="w-stats">423,964</p>
-                            </div>
-                            <div class="w-chart-render-one">
-                                <div id="total-users"></div>
+                            <div class="mb-4 pb-2">
+                                <p class="w-title">
+                                    @if($item->credito == 1)
+                                        Créditos
+                                    @else 
+                                        Débitos
+                                    @endif
+                                </p>
+                                <p class="w-stats">R$ {{number_format($item->valor,2,',','.')}}</p>
                             </div>
                         </div>
-
-                        <div class="w-chart-section">
-                            <div class="w-detail">
-                                <p class="w-title">Paid Visits</p>
-                                <p class="w-stats">7,929</p>
-                            </div>
-                            <div class="w-chart-render-one">
-                                <div id="paid-visits"></div>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
@@ -80,19 +75,7 @@
                 <div class="widget widget-chart-three">
                     <div class="widget-heading">
                         <div class="">
-                            <h5 class="">Unique Visitors</h5>
-                        </div>
-
-                        <div class="dropdown  custom-dropdown">
-                            <a class="dropdown-toggle" href="#" role="button" id="uniqueVisitors" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="uniqueVisitors">
-                                <a class="dropdown-item" href="javascript:void(0);">View</a>
-                                <a class="dropdown-item" href="javascript:void(0);">Update</a>
-                                <a class="dropdown-item" href="javascript:void(0);">Download</a>
-                            </div>
+                            <h5 class="">Evolução do caixa</h5>
                         </div>
                     </div>
 
@@ -101,6 +84,75 @@
                     </div>
                 </div>
             </div>
+            <script>
+  var d_1options1 = {
+    chart: {
+      height: 350,
+      type: 'bar',
+      toolbar: {
+        show: false,
+      },
+      dropShadow: {
+        enabled: true,
+        top: 1,
+        left: 1,
+        blur: 2,
+        color: '#acb0c3',
+        opacity: 0.7,
+      }
+    },
+    colors: ['#5c1ac3', '#ffbb44'],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '55%',
+        endingShape: 'rounded'
+      },
+    },
+    dataLabels: {
+      enabled: false
+    },
+    legend: {
+      show: false
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ['transparent']
+    },
+    series: [{
+      name: 'Créditos',
+      data: ['{!!implode("','",($crescimento_operacoes[1]))!!}']
+    }, {
+      name: 'Débitos',
+      data: ['{!!implode("','",($crescimento_operacoes[0]))!!}']
+    }],
+    xaxis: {
+      categories: ['{!!implode("','",array_keys($crescimento_operacoes[0]))!!}'],
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'light',
+        type: 'vertical',
+        shadeIntensity: 0.3,
+        inverseColors: false,
+        opacityFrom: 1,
+        opacityTo: 0.8,
+        stops: [0, 100]
+      },
+    },
+    colors:['#F44336', '#0037ff'],
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          var f = val.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+          return f
+        }
+      }
+    }
+  }
+            </script>
 
             <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
                 <div class="widget widget-activity-three">
@@ -209,22 +261,22 @@
             <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
                 <div class="widget-four">
                     <div class="widget-heading">
-                        <h5 class="">Visitors by Browser</h5>
+                        <h5 class="">Associados</h5>
                     </div>
                     <div class="widget-content">
                         <div class="vistorsBrowser">
-                            <div class="browser-list">
+
+                        <div class="browser-list">
                                 <div class="w-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chrome"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="4"></circle><line x1="21.17" y1="8" x2="12" y2="8"></line><line x1="3.95" y1="6.06" x2="8.54" y2="14"></line><line x1="10.88" y1="21.94" x2="15.46" y2="14"></line></svg>
-                                </div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>                                </div>
                                 <div class="w-browser-details">
                                     <div class="w-browser-info">
-                                        <h6>Chrome</h6>
-                                        <p class="browser-count">65%</p>
+                                        <h6>Associados não conveniados</h6>
+                                        <p class="browser-count">{{number_format((($ass_nconveniados/$ass_total)*100),1,',')}}%</p>
                                     </div>
                                     <div class="w-browser-stats">
                                         <div class="progress">
-                                            <div class="progress-bar bg-gradient-primary" role="progressbar" style="width: 65%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar bg-gradient-primary" role="progressbar" style="width: {{(($ass_nconveniados/$ass_total)*100)}}%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                     </div>
                                 </div>
@@ -232,45 +284,20 @@
 
                             <div class="browser-list">
                                 <div class="w-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-compass"><circle cx="12" cy="12" r="10"></circle><polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon></svg>
-                                </div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>                                </div>
                                 <div class="w-browser-details">
-
                                     <div class="w-browser-info">
-                                        <h6>Safari</h6>
-                                        <p class="browser-count">25%</p>
+                                        <h6>Associados conveniados</h6>
+                                        <p class="browser-count">{{number_format((($ass_conveniados/$ass_total)*100),1,',')}}%</p>
                                     </div>
-
                                     <div class="w-browser-stats">
                                         <div class="progress">
-                                            <div class="progress-bar bg-gradient-danger" role="progressbar" style="width: 35%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                            <div class="progress-bar bg-gradient-primary" role="progressbar" style="width: {{(($ass_conveniados/$ass_total)*100)}}%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                                         </div>
                                     </div>
-
                                 </div>
-
                             </div>
 
-                            <div class="browser-list">
-                                <div class="w-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>
-                                </div>
-                                <div class="w-browser-details">
-
-                                    <div class="w-browser-info">
-                                        <h6>Others</h6>
-                                        <p class="browser-count">15%</p>
-                                    </div>
-
-                                    <div class="w-browser-stats">
-                                        <div class="progress">
-                                            <div class="progress-bar bg-gradient-warning" role="progressbar" style="width: 15%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                            </div>
 
                         </div>
 
@@ -282,49 +309,31 @@
                 <div class="row widget-statistic">
                     <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="widget widget-one_hybrid widget-followers">
-                            <div class="widget-heading">
+                            <div class="widget-heading mb-5 pb-5">
                                 <div class="w-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                                </div>
-                                <p class="w-value">31.6K</p>
-                                <h5 class="">Followers</h5>
-                            </div>
-                            <div class="widget-content">
-                                <div class="w-chart">
-                                    <div id="hybrid_followers"></div>
-                                </div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>                                </div>
+                                <p class="w-value">{{$ass_total}}</p>
+                                <h5 class="mb-2">Associados</h5>
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="widget widget-one_hybrid widget-referral">
-                            <div class="widget-heading">
+                            <div class="widget-heading mb-5 pb-5">
                                 <div class="w-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>
-                                </div>
-                                <p class="w-value">1,900</p>
-                                <h5 class="">Referral</h5>
-                            </div>
-                            <div class="widget-content">
-                                <div class="w-chart">
-                                    <div id="hybrid_followers1"></div>
-                                </div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>                                </div>
+                                <p class="w-value">{{$ass_nconveniados}}</p>
+                                <h5 class="mb-2">Associados não conveniados</h5>
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
                         <div class="widget widget-one_hybrid widget-engagement">
-                            <div class="widget-heading">
+                            <div class="widget-heading mb-5 pb-5">
                                 <div class="w-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
-                                </div>
-                                <p class="w-value">18.2%</p>
-                                <h5 class="">Engagement</h5>
-                            </div>
-                            <div class="widget-content">
-                                <div class="w-chart">
-                                    <div id="hybrid_followers3"></div>
-                                </div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>                                </div>
+                                <p class="w-value">{{$ass_conveniados}}</p>
+                                <h5 class="mb-2">Associados conveniados</h5>
                             </div>
                         </div>
                     </div>
