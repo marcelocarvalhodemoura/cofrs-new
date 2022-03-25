ddf5f6885134d5d2f247b373c8bb122334b0178e
John <jonatascrizel@gmail.com>
2022-03-25 10:16:10 -0300

    alteração do local da mensagem de erro de login

--- public/assets/js/authentication/form-1.js
+++ public/assets/js/authentication/form-1.js
@@ -3,38 +3,38 @@ $('.form-content').fadeIn('slow');
 var togglePassword = document.getElementById("toggle-password");
 
 if (togglePassword) {
-	togglePassword.addEventListener('click', function() {
-	  var x = document.getElementById("password");
-	  if (x.type === "password") {
-	    x.type = "text";
-	  } else {
-	    x.type = "password";
-	  }
-	});
+  togglePassword.addEventListener('click', function () {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  });
 }
-$('#formAuth').on('submit', event =>{
-    event.preventDefault();
+$('#formAuth').on('submit', event => {
+  event.preventDefault();
 
-    $.ajax({
-        method:"POST",
-        url:"/auth",
-        data:$("#formAuth").serialize(),
-        success: response => {
-            console.log(response.status);
-            if( response.status == 'error' ){
-                const msg = '<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Atenção!</strong> ' + response.msg +
-                    '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-                    '<span aria-hidden="true">&times;</span>' +
-                    '</button>' +
-                    '</div>';
+  $.ajax({
+    method: "POST",
+    url: "/auth",
+    data: $("#formAuth").serialize(),
+    success: response => {
+      console.log(response.status);
+      if (response.status == 'error') {
+        const msg = '<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Atenção!</strong> ' + response.msg +
+          '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+          '<span aria-hidden="true">&times;</span>' +
+          '</button>' +
+          '</div>';
 
-                $('#formAuth').append(msg);
-            }else{
-                window.location.href = '/dashboard';
-            }
+        $('#formAuth').prepend(msg);
+      } else {
+        window.location.href = '/dashboard';
+      }
 
-        }
-    });
+    }
+  });
 
 
 });
