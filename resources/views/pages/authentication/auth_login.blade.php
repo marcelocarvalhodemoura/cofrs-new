a77f8bacbc5f19c0b579270fe14dc5fb0892c86b
John <jonatascrizel@gmail.com>
2022-04-01 19:17:04 -0300

    Removido esqueci minha senha

--- resources/views/pages/authentication/auth_login.blade.php
+++ resources/views/pages/authentication/auth_login.blade.php
@@ -23,7 +23,9 @@
                                 <div id="password-field" class="field-wrapper input mb-2">
                                     <div class="d-flex justify-content-between">
                                         <label for="password">SENHA</label>
+                                        <!--
                                         <a href="/authentication/pass_recovery_boxed" class="forgot-pass-link"  tabindex="1">Esqueceu a senha?</a>
+-->
                                     </div>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
                                     <input id="password" name="password" type="password" class="form-control" placeholder="Senha" tabindex="0" />
