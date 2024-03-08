e0ddeeb395ece2efc418119cd2a5940e19844101
John <jonatascrizel@gmail.com>
2024-03-08 08:48:23 -0300

    edição status parcela

--- resources/views/covenants/modal/editStatusParcela.blade.php
+++ resources/views/covenants/modal/editStatusParcela.blade.php
@@ -1,4 +1,4 @@
-<button type="button" id="btnEditStatusPortion" class="btn btn-primary bs-popover rounded mb-2 mr-2" data-placement="top"  data-container="body" data-trigger="hover" data-content="Editar valor da parcela" data-target="#convenantModalEditStatusParcela">
+<button type="button" id="btnEditStatusPortion" class="btn btn-primary bs-popover rounded mb-2 mr-2" data-placement="top"  data-container="body" data-trigger="hover" data-content="Editar status da parcela" data-target="#convenantModalEditStatusParcela">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shuffle"><polyline points="16 3 21 3 21 8"></polyline><line x1="4" y1="20" x2="21" y2="3"></line><polyline points="21 16 21 21 16 21"></polyline><line x1="15" y1="15" x2="21" y2="21"></line><line x1="4" y1="4" x2="9" y2="9"></line></svg>
 </button>
 
