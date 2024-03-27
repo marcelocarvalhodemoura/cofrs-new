7e2b14ac3c8418e600f6b29bba3803ef6caa1cae
John <jonatascrizel@gmail.com>
2024-03-27 11:00:06 -0300

    edição de parcela

--- app/Http/Controllers/ConvenantController.php
+++ app/Http/Controllers/ConvenantController.php
@@ -853,7 +853,7 @@ class ConvenantController extends Controller
             $parcelas_afetadas = Portion::whereIn('id', json_decode($request->idparcelas))
                 ->whereIn('par_status', ['Pendente','Renegociado','Reparcelado','Transferido','Vencido'])
                 ->update([
-                    'par_valor' => str_replace(',','.', $request->valor),
+                    'par_valor' => str_replace(',','.', str_replace('.','', $request->valor)),
                 ]);
 
             //edita o lançamento
