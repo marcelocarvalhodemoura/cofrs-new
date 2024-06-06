d177f9f6373e4af9a5339fef4e457c4496530424
John <jonatascrizel@gmail.com>
2024-06-06 10:09:10 -0300

    Associados > desativar

--- resources/views/associate/form/_associate-form-line8.blade.php
+++ resources/views/associate/form/_associate-form-line8.blade.php
@@ -9,11 +9,11 @@
 
     <div class="col-md-4 mb-4">
         <label for="assoc_dataativacao">Data de ativação</label>
-        <input type="text" class="form-control" id="assoc_dataativacao" name="assoc_dataativacao" placeholder="99/99/9999" readonly>
+        <input type="text" class="form-control" id="assoc_dataativacao" name="assoc_dataativacao" placeholder="99/99/9999" readonly />
     </div>
     <div class="col-md-4 mb-4">
         <label for="assoc_datadesligamento">Data de desligamento</label>
-        <input type="text" class="form-control" id="assoc_datadesligamento" name="assoc_datadesligamento" placeholder="99/99/9999"  readonly>
+        <input type="text" class="form-control" id="assoc_datadesligamento" name="assoc_datadesligamento" placeholder="99/99/9999" />
     </div>
 
 </div>
